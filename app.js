6550fe0a164c1a9f2095aaf010acba831de3197a
kachilala <kachilala23@gmail.com>
2020-11-27 11:34:44 +0100

    delete connection string in app.js

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-// const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => app.listen(3000))
-//   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
